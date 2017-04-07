73d0f4fabfe30907dd0915c7e0ef141d4181d4fa
Qwerty Qwerty <Qwerty Qwerty>
2017-04-07 16:56:04 +0300

    added navigation-drawer activity

--- app/src/main/java/com/project/mytasktracker/MainActivity.java
+++ app/src/main/java/com/project/mytasktracker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.project.mytasktracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Collection;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, NavigationDrawerActivity.class);
+        startActivity(intent);
     }
 }
